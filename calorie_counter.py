3bf4f60e27d4180d5f3fea4ead25aec5c53854c4
Quan Viet Pham <qpham@putneyschool.org>
2018-05-25 16:46:25 -0400

    User can now insert their information

--- calorie_counter.py
+++ calorie_counter.py
@@ -21,7 +21,7 @@ class User:
         if self.gender == "f":
             User.rate = 655 + (4.35 * self.weight) + (4.7 * self.height) - (4.7 * self.age)
         # print the user's bmr
-        print(User.rate)
+        print("Without any activity, your body burns %s calories everyday" % User.rate)
     # calculate the user's total daily calorie needs using the Harris Benedict equation
     def daily_cal(self):
         # if the user is sedentary, use this formula
@@ -40,14 +40,22 @@ class User:
         if self.activity == 5:
             User.need = User.rate * 1.9
         # print the user's total daily calorie needs
-        print(User.need)
+        print("You need %s calories everyday in order to maintain your current weight" % User.need)
 
-# user information
-quan = User("Quan", 18, "m", 132, 68, 3)
+# user inserts their information
+name = input("What is your name?")
+age = int(input("How old are you?"))
+gender = input("What is your gender? (m/f)")
+weight = int(input("Enter your weight (pound)"))
+height = int(input("Enter your height (inch)"))
+activity = int(input("How active are you? (1: little or no exercise; 2: light exercise 1-3 days/week; 3: moderate exercise 3-5 days/week; 4: hard exercise 6-7 days/week; 5: very hard exercise & physical job or 2x training)"))
+user = User(name, age, gender, weight, height, activity)
 
-# calculate this user's bmr
-quan.bmr()
-quan.daily_cal()
+# calculate this user's bmr and total daily calorie needs
+user.bmr()
+user.daily_cal()
+
+
 
 
 
